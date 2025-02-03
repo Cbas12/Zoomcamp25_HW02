07bc3a024d54b7c6089426ac90e451c9dab54e94
Cbas12 <sebastianperalta@hotmail.com>
2025-02-02 23:19:00 -0500

    fin

--- selects.sql
+++ selects.sql
@@ -1,1 +1,10 @@
-se
+--q3
+select count(1) as filas from public.yellow_tripdata where filename like '%2020%';
+
+
+--q4
+select count(1) as filas from public.green_tripdata where filename like '%2020%';
+
+
+--q5
+select filename,count(1) as filas from public.yellow_tripdata group by filename;
